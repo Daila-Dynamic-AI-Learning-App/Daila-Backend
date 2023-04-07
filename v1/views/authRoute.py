72fa8ee21ffe663ba1db1b09abe3220718f4738a
tommydebisi <tommydebisi@gmail.com>
2023-04-07 12:24:38 +0100

    :seedling: tweaked error msgs of login and signup

--- v1/views/authRoute.py
+++ v1/views/authRoute.py
@@ -20,14 +20,14 @@ def registerUser():
         abort(400)
 
     if not validateEmail(email):
-        abort(400)
+        return jsonify(error="Incorrect email pattern"), 400
 
     try:
         AUTH.registerUser(obj)
         return jsonify({ 'message': 'success' }), 202
     except ValueError:
         # user already exists
-        abort(400)
+        return jsonify(error="User exists"), 400
 
 @daila.route('/login', methods=['POST'], strict_slashes=False)
 def loginUser():
@@ -42,14 +42,13 @@ def loginUser():
 
     email = data.get('email')
     if not validateEmail(email):
-        abort(400)
+        return jsonify(error="Incorrect email pattern"), 400
 
     try:
         token = AUTH.loginUser(data)
-
         return jsonify(token=token), 200
     except ValueError:
-        abort(400)
+        return jsonify(error="Incorrect email or password"), 401
 
 @daila.route('/logout', methods=['GET'], strict_slashes=False)
 def logoutUser():
